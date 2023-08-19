d474ac2051b0fe5286df8764013d2caf595e3db9
r3d5un <r3d5un@pm.me>
2023-08-19 19:37:44 +0200

    Update in app docs

--- cmd/root.go
+++ cmd/root.go
@@ -27,21 +27,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pScan.islandwind.me",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Fast TCP pot scannner",
+	Long: `pScan - short for Port Scanner - executes TCP port scan
+on a list of hosts.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+pScan allows you to add, list, and delete hosts from the list.
+
+pScan executes a port scan on specified TCP ports. You can customize the
+target ports using a command line flag.`,
+	Version: "0.1",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -63,6 +60,7 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+	rootCmd.SetVersionTemplate(versionTemplate)
 }
-
-
