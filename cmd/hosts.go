9d96e63bc88b072bdef601d5e3a8d6be80fb6249
r3d5un <r3d5un@pm.me>
2023-08-20 18:08:50 +0200

    Add port scanning functionality

--- cmd/hosts.go
+++ cmd/hosts.go
@@ -22,8 +22,6 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -36,9 +34,6 @@ var hostsCmd = &cobra.Command{
 Add hosts with the add command
 Delete hosts with the delete command
 List hosts with the list command`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hosts called")
-	},
 }
 
 func init() {
